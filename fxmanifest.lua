5faef544edbed30a3f30afef83288cb9467f911f
Infernalz <51412133+Infernalz92@users.noreply.github.com>
2023-10-07 18:58:44 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,5 @@
+fx_version 'cerulean'
 game 'rdr3'
-fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 author 'Infernalz'
@@ -7,9 +7,7 @@ description 'qbr-mining'
 version '1.0.0'
 lua54 'yes'
 
-client_script {
-    'client/*.lua'
-}
+client_script {'client/*.lua'}
 
 server_scripts {'server/*.lua'}
 
